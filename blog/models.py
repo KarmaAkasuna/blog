4f9b643d7bdbd737078b69ee5b524e8be0f089ab
Karma <wolfy5613@gmx.de>
2017-11-18 11:38:03 +0100

    My App, first commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField("Title (max. length 200)", max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    dings = models.TextField("^^")
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    def _str_(self):
+        return self.title
