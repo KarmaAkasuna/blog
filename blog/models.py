5aedff2d18110503b038f6f4b898353f1a996d8a
Karma <wolfy5613@gmx.de>
2017-11-18 16:51:12 +0100

    SVG

--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,5 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    def _str_(self):
+    def __str__(self):
         return self.title
