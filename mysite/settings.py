e7a36df4fea3fe729917bb01270b681d396518f4
Karma <wolfy5613@gmx.de>
2017-11-18 12:17:51 +0100

    change ALLOWED_HOSTS because of bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'suh#1iw9$+pgp@!ld2=hpnba4l=!-@%#qlginio(52vl@p8b5j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["karmaakasuna.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
