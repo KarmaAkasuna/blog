d092be85ea23969916e3c5e21bd8aa9abb1eca0c
Karma <wolfy5613@gmx.de>
2017-11-18 12:24:45 +0100

    Fix bug

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,6 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    dings = models.TextField("^^")
     def publish(self):
         self.published_date = timezone.now()
         self.save()
